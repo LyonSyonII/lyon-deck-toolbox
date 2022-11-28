3865c2dc172d23d542d365acb3c20378d42a4359
LyonSyonII <liam.garriga@tutanota.com>
2022-11-28 16:08:37 +0100

    First

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ impl eframe::App for App {
 
             ui.group(|ui| {
                 ScrollArea::vertical().show(ui, |ui| {
-                    tool(ui, "Rwfus", "Creates an overlay over the Root filesystem that allows <code>pacman</code> to install packages with SteamOS readonly enabled.", &mut self.rwfus);
+                    tool(ui, "Rwfus", "Read-Write OverlayFS for your Steam Deck!\nCreates an overlay over the Root filesystem that allows <code>pacman</code> to install packages with SteamOS readonly enabled.", &mut self.rwfus);
                     tool(ui, "Rwfus", "Creates an overlay over the Root filesystem that allows <code>pacman</code> to install packages with SteamOS readonly enabled.", &mut self.rwfus);
                     tool(ui, "Rwfus", "Creates an overlay over the Root filesystem that allows <code>pacman</code> to install packages with SteamOS readonly enabled.", &mut self.rwfus);
                     tool(ui, "Rwfus", "Creates an overlay over the Root filesystem that allows <code>pacman</code> to install packages with SteamOS readonly enabled.", &mut self.rwfus);
