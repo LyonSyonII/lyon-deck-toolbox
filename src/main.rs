cf614211a63eb373745d73e1abf56560a6876af8
LyonSyonII <liam.garriga@tutanota.com>
2022-11-28 14:22:40 +0100

    First

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct App;
 
 impl App {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        cc.egui_ctx.set_heading_font_style(18., eframe::epaint::FontFamily::Proportional);
+        cc.egui_ctx.set_heading_font_style(54., eframe::epaint::FontFamily::Proportional);
         //cc.egui_ctx.set_small_font_style(8., eframe::epaint::FontFamily::Proportional); // TODO! Bug!!!!
         cc.egui_ctx.set_visuals(ui::Visuals::light());
         Self::default()
