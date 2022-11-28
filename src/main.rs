2e17a3e2aee156f1636f60730b58a287c4ab7adc
LyonSyonII <liam.garriga@tutanota.com>
2022-11-28 16:00:04 +0100

    First

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ impl eframe::App for App {
         });
         
         ui::CentralPanel::default().show(ctx, |ui| {
-            ui.vertical(|ui| {
+            ui.vertical_centered(|ui| {
                 ui.label(RichText::new("Steam Deck Tools").underline().heading());
                 ui.label(RichText::new("Select the tools you want to install, or click 'Install All'.").size(5.));
             });
