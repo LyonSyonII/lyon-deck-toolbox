19727392a725e14810353e70109308f487e145bc
LyonSyonII <slimystudios@protonmail.com>
2022-11-28 11:51:48 +0100

    First

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ impl eframe::App for App {
         ui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered(|ui| {
                 ui.heading("Steam Deck Tools");
-                ui.label("Select the tools you want to install, or click 'Install All'.\n").on_hover_text("Tooltip!");
+                ui.label("Select the tools you want to install, or click 'Install All'.\nHover the mouse over each entry to see a quick explanation of what each one does.").on_hover_text("Tooltip!");
             });
         });
     }
