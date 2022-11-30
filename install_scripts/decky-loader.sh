e338f2bc68d5c1a369ea7bbcddb66d22b3013769
LyonSyonII <liam.garriga@tutanota.com>
2022-11-30 16:17:17 +0100

    Added decky loader

---
+++ install_scripts/decky-loader.sh
@@ -0,0 +1,6 @@
+curl -L https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_release.sh | sh
+
+echo &&
+read -n 1 -r -s -p "Installation completed, press any key to close the window..." &&
+
+exit 0;
