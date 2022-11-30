0a9c2f8e76baa17fdae3647261c4a332fa9830ac
LyonSyonII <liam.garriga@tutanota.com>
2022-11-30 15:54:58 +0100

    Checking for passsword set

--- install_scripts/needs_root.sh
+++ install_scripts/needs_root.sh
@@ -1,6 +1,5 @@
-if [[ $(passwd -S) == *" NP "* ]]; then
+if [[ $(passwd -S) == *" P "* ]]; then
     echo 'This utility REQUIRES a password set on the Steam Deck.'
     echo 'A password will be set now:'
+    passwd
 fi
-
-
