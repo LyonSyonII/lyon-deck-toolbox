71cba1a953ee3e403e66027ae7e6bc30aa79bb6a
LyonSyonII <slimystudios@protonmail.com>
2022-12-01 14:11:00 +0100

    Replaced 'curl' crate with call to native curl

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
-# RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
-cargo build --release
+RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
+#cargo build --release
 cp target/release/steam-deck-tools steam-deck-tools
 git commit -am "$1"
 git push
