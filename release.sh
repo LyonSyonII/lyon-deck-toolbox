249fb528f1f721054da0d662c641bdab38b90149
LyonSyonII <slimystudios@protonmail.com>
2022-12-01 13:36:55 +0100

    Executable is now statically linked

--- release.sh
+++ release.sh
@@ -1,2 +1,5 @@
-RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
-cp target/x86_64-unknown-linux-gnu/release/steam-deck-tools steam-deck-tools
+# RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
+cargo build --release
+cp target/release/steam-deck-tools steam-deck-tools
+git commit -am "$1"
+git push
