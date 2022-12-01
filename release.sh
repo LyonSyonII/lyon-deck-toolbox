9fd67815d0a057eb58ef0b007c9e287fa7daefc5
LyonSyonII <slimystudios@protonmail.com>
2022-12-01 13:30:05 +0100

    added static linking

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
-cp target/release/steam-deck-tools steam-deck-tools
+cp target/x86_64-unknown-linux-gnu/release/steam-deck-tools steam-deck-tools
