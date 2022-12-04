f9da94bd1e7e194ed3420ebbe79f08023ee9091f
LyonSyonII <liam.garriga@tutanota.com>
2022-12-04 19:49:04 +0100

    Added latest executable

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
-RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
+#RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
 #cargo build --release
+RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --target x86_64-unknown-linux-musl
 cp target/release/steam-deck-tools steam-deck-tools
 git commit -am "$1"
 git push
