28cb332d4f0879199a41776aa8f8ac748660bec5
LyonSyonII <slimystudios@protonmail.com>
2022-12-11 22:13:27 +0100

    Added SteamOS btrfs

--- install_scripts/steamosbtrfs.sh
+++ install_scripts/steamosbtrfs.sh
@@ -16,7 +16,7 @@ chmod u+x install.sh
 ./install.sh
 
 EXIT=$?
-if [ EXIT -eq 0 ]
+if [ $EXIT -eq 0 ]
 then
     echo $EXIT;
     read -n 1 -r -s -p "Installation completed, press any key to close the window..."
